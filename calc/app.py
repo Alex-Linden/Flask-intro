c6f43b18bd78d1b19a41bab524b004b170b46fc2
Robin Sheen <robinnsheen@berkeley.edu>
2022-06-08 12:11:36 -0700

    finished app.py

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,15 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add
+
+
+app = Flask(__name__)
+
+@app.get("/add")
+def return_add():
+    """Handle GET requests like /add?a=5&b=4"""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    sum = add(a,b)
+    return f"<h1>{a} + {b} = {sum}</h1>"
