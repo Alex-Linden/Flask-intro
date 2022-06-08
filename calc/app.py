1db1ada0fba086ec6997b20f8810b4ba710b802f
Robin Sheen <robinnsheen@berkeley.edu>
2022-06-08 12:19:55 -0700

    finished operations

--- calc/app.py
+++ calc/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from operations import add
+from operations import add, sub, mult, div
 
 
 app = Flask(__name__)
@@ -11,5 +11,31 @@ def return_add():
     a = int(request.args["a"])
     b = int(request.args["b"])
 
-    sum = add(a,b)
-    return f"<h1>{a} + {b} = {sum}</h1>"
+    return f"<h1>{a} + {b} = {add(a,b)}</h1>"
+
+@app.get("/sub")
+def return_sub():
+    """Handle GET requests like /sub?a=5&b=4"""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return f"<h1>{a} - {b} = {sub(a,b)}</h1>"
+
+@app.get("/mult")
+def return_mult():
+    """Handle GET requests like /mult?a=5&b=4"""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return f"<h1>{a} * {b} = {mult(a,b)}</h1>"
+
+@app.get("/div")
+def return_div():
+    """Handle GET requests like /div?a=4&b=2"""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return f"<h1>{a} / {b} = {div(a,b)}</h1>"
